e63f4e8861664d2e4471be697cdcf99894d050fb
syg <sgogel@ecedi.fr>
2014-10-02 18:23:51 +0200

    refactore source tracking

--- utmz.js
+++ utmz.js
@@ -19,69 +19,108 @@
 
             return params;
         };
+        
+        var _parseUrl = function(url) {
+            var parser = document.createElement('a'),
+            searchObject = {},
+            queries, split, i;
+            // Let the browser do the work
+            
+            parser.href = url;
+            queries = parser.search.replace(/^\?/, '').split('&');
 
-        //get main domain (do not distinct subdomains)
-        var _getDomain = function(domain) {
-            var docdomain = domain.split('.');
-            var dom1 = '';
-            if (typeof (docdomain[docdomain.length - 2]) != 'undefined') dom1 = docdomain[docdomain.length - 2] + '.';
-            var domain = dom1 + docdomain[docdomain.length - 1];
+            for( i = 0; i < queries.length; i++ ) {
+                split = queries[i].split('=');
+                searchObject[split[0]] = split[1];
+            }
 
-            return domain;
+            return {
+                protocol: parser.protocol,
+                host: parser.host,
+                hostname: parser.hostname,
+                port: parser.port,
+                pathname: parser.pathname,
+                search: parser.search,
+                searchObject: searchObject,
+                hash: parser.hash
+            };
         };
 
         //utmz code
-        console.log('in utmz');
+        var oldCookie = {source : false, campaign: false, medium: false};
 
-        var oldCookie = JSON.parse($.cookie('__utmze'));
+        if($.cookie('__utmze')) {
+            oldCookie = JSON.parse($.cookie('__utmze'));
+        }
         
-        console.log("read cookie");
+        console.log('read cookie');
         console.log(oldCookie);
 
         var get = _getQueryParams(document.location.search);
         
-        var _utmz = {source : oldCookie.source, campaign: oldCookie.campaign };
+        var _utmz = {source : oldCookie.source, campaign: oldCookie.campaign, medium: oldCookie.medium };
 
         console.log(get);
 
+        var myDomain = _parseUrl(document.domain).hostname;
 
         // # source 
         // soit il est dans l'url et il prime
         // sinon on prend le domain issue du referrer
         // si le referrer est vide, on met (organic) par defaut
-        // TODO mais on n'écrase pas le referrer si c le meme que le domain courant
-
-        if(get.hasOwnProperty('source') ) {
-            _utmz.source = get.source;
+        if(get.hasOwnProperty('utm_source') ) {
+            _utmz.source = get.utm_source;
         } else {
-            _utmz.source = _getDomain(document.referrer);
+            var referrerDomain = _parseUrl(document.referrer).hostname;
+
+            //find source source is referrer if not same domain
+            if( referrerDomain === myDomain) {
+                _utmz.source = oldCookie.source || false;
+            } else {
+                _utmz.source = referrerDomain;
+            }
+
+            //si pas de referrer on considère organic
+            if(_utmz.source === '') {
+                _utmz.source = '(direct)';
+            }
+
+            if(_utmz.source === false) {
+                _utmz.source = '(organic)';
+            }
+            
         }
 
-        //si pas de referrer on considère organic
-        if(_utmz.source === '' || _utmz.source === false) {
-            _utmz.source = '(organic)';
+
+        // #campaign OK
+        if(get.hasOwnProperty('utm_campaign')) {
+            _utmz.campaign = get.utm_campaign;
         }
 
-        var myDomain = _getDomain(document.domain);
-
-        //find source source is referrer if not same domain
-        if(_utmz.source != myDomain) {
-            //_utmz.source = _utmz.source;
+        // medium on écrase le medium si il est dans l'url
+        if(get.hasOwnProperty('utm_medium')) {
+            _utmz.medium = get.utm_medium;
         }
 
-        // #campaign
-        //fins campaign, from location hash
-        if(get.hasOwnProperty('campaign')) {
-            _utmz.campaign = get.campaign;
-        }
+
+        var _listSubDomains = function(domain) {
+            var doms = domain.split('.');
+            console.log(doms);
+
+        };
 
         $.cookie.json = true;
         //write cookie for 6 month
-        $.cookie('__utmze', _utmz, { expires: 182, domain: myDomain });
+        //TODO try to write on lower domains first
+        var writeDomains = _listSubDomains(myDomain);
         
-        console.log("write cookie");
+        console.log('write cookie');
         console.log(_utmz);
 
+        $.cookie('__utmze', _utmz, { expires: 182, domain: '.loc' });
+        
+
+        
         return this;
     };
 }(jQuery));
